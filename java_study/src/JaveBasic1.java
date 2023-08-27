96c2ede3a400adb5d88483aeba27b565554f7e8f
Jo0307 <seongyeun.jo@gmail.com>
2023-08-27 16:43:41 +0200

    27.08.23

---
+++ java_study/src/JaveBasic1.java
@@ -0,0 +1,15 @@
+public class JavaBasic1 {
+    public static void main(String [] args){
+
+    }
+
+    public static void signedIntegerType(){
+        int num_dec = -1;
+        int num_hex = 0xFFFFFFFF;
+        int num_bin = 0b11111111111111111111111111111111;
+
+        System.out.println("num_dec = " + num_dec);
+        System.out.println("num_hex = " + num_hex);
+        System.out.println("num_bin = " + num_bin);
+    }
+}
